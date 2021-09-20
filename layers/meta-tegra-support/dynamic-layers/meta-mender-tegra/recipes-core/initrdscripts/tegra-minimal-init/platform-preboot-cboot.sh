4943ae647e109e54170d89d8a526eae64527f84b
Tiran Kaskas <tirank@juganu.com>
2021-09-20 18:18:10 +0300

    fixing commit per procedures
    Signed-off-by: Tiran Kaskas <tirank@juganu.com>

--- layers/meta-tegra-support/dynamic-layers/meta-mender-tegra/recipes-core/initrdscripts/tegra-minimal-init/platform-preboot-cboot.sh
+++ layers/meta-tegra-support/dynamic-layers/meta-mender-tegra/recipes-core/initrdscripts/tegra-minimal-init/platform-preboot-cboot.sh
@@ -2,18 +2,18 @@ slotsfx=""
 mayberoot=""
 for bootarg in `cat /proc/cmdline`; do
     case "$bootarg" in
-	boot.slot_suffix=*) slotsfx="${bootarg##boot.slot_suffix=}" ;;
-	root=*) mayberoot="${bootarg##root=}" ;;
-	ro) opt="ro" ;;
-	rootwait) wait="yes" ;;
+      boot.slot_suffix=*) slotsfx="${bootarg##boot.slot_suffix=}" ;;
+      root=*) mayberoot="${bootarg##root=}" ;;
+      ro) opt="ro" ;;
+      rootwait) wait="yes" ;;
     esac
 done
 rootdev=`blkid -l -t PARTLABEL=APP$slotsfx | cut -d: -f1`
 if [ -z "$rootdev" ]; then
     if [ -n "$mayberoot" ]; then
-	rootdev="$mayberoot"
+        rootdev="$mayberoot"
     else
-	rootdev="/dev/mmcblk0p1"
+        rootdev="/dev/mmcblk0p1"
     fi
 fi
 
