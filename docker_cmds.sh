948356d3de666af47b875fd605e94722807c71e4
sam <sam@distributedspectrum.com>
2024-03-24 09:59:08 -0400

    add the commands to build the docker container and run it and stuff

---
+++ docker_cmds.sh
@@ -0,0 +1,3 @@
+export DOCKER_BUILDKIT=1
+docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) --build-arg UNAME=sam --ssh default -t yocto-build-env .
+docker run -it --rm -v $(pwd):/yocto -v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK yocto-build-env
