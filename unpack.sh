b99ad84d6ff497c5128679042b27d64068fd9d95
sam wolfson <sam@distributedspectrum.com>
2024-03-27 13:58:55 -0400

    add scripts to handle the fs image and add hostname and ssh key and stuff

---
+++ unpack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+image=ds-image-sato
+machine=jetson-xavier-nx-devkit-emmc
+builddir=build
+
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+deployfile=$(realpath ${scriptdir}/${builddir}/tmp/deploy/images/${machine}/${image}-${machine}.tegraflash.tar.gz)
+ 
+#tmpdir=`mktemp`
+tmpdir=/tmp/ds-flash-${machine}
+
+rm -rf $tmpdir
+mkdir -p $tmpdir
+echo "Using temp directory $tmpdir"
+pushd "$tmpdir"
+cp $deployfile .
+tar -xvf $deployfile
+set -e
